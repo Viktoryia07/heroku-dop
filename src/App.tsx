aed58fc8ba2fd7168092e7ca74815983af7e867e
Viktoryia07 <viktoriyarybik@gmail.com>
2022-11-30 17:26:44 +0300

    Commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {json} from "stream/consumers";
+
+type PropsType = {
+    body: string
+    id: number
+    title: string
+    userId: number
+}
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState<PropsType[]>([])
+    const addHandler = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setState(json))
+    }
+    const deleteHandler = () => {
+        setState([])
+    }
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setState(json))
+    }, [])
+    return (
+        <div className="App">
+            <button onClick={addHandler}>Show posts</button>
+            <button onClick={deleteHandler}>Delete</button>
+            <ul>
+                {state.map((el) => {
+                    return (
+                        <li key={el.id}>
+                         <span>{el.id}</span>
+                         <span>{el.userId}</span>
+                         <span>{el.title}</span>
+
+                        </li>
+
+                    )
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
